32fff591773ff1f8b3b57bb8fccdb763fbbbafb4
Saketh <saketharipirala101@gmail.com>
2021-11-12 22:37:24 +0200

    First route

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const path = require('path')
 const app = express();
 
 app.use("/public", express.static(path.join(__dirname,'public')))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,'public/index.html'))
+})
+
+app.listen(3000);
