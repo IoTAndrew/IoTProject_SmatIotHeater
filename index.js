d27890aa6978a573c93f006358e85da455ef006c
Saketh <saketharipirala101@gmail.com>
2021-11-16 13:53:39 +0200

    AuthHTML

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const path = require('path')
 const app = express();
 
+
 app.use("/public", express.static(path.join(__dirname,'public')))
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname,'public/index.html'))
+    res.sendFile(path.join(__dirname,'public/auth.html'))
 })
 
 app.listen(3000);
