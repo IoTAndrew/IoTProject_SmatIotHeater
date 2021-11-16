8d0b789ce5a1f7d1c4d74e7277094c7d89d556e3
Saketh <saketharipirala101@gmail.com>
2021-11-16 15:27:36 +0200

    HTML

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,'public/auth.html'))
 })
 
+app.get("/home", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+app.get("/create", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/user.html'))
+})
+
 app.listen(3000);
 
-//test
